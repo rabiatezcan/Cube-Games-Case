99a9544808da74320c07dfd7a405b94043dd9caf
rabiatezcan <rabiatezcan@hotmail.com>
2021-09-02 11:21:43 +0300

    feat : constant variables initialized.

--- Projectile Motion/Assets/Scripts/Constants.cs
+++ Projectile Motion/Assets/Scripts/Constants.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Constants
 {
-    public const float amountOfMovement = 10;
+    public const float amountOfMovement = 10f;
+    public const int poolDepth = 16;
+    public const int maxPoolDepth = 8;
+    public const float gravitationalForce = 9.81f;
+    public const float yAxisOffset = 3.2f;
 }
 
